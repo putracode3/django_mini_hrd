db179625630fdcf999545be1b0d25135450afc62
dennysw <dennysetiawisnugraha@gmail.com>
2018-03-02 00:47:36 +0700

    add comment for connection mysql db

--- mini_hrd/settings.py
+++ mini_hrd/settings.py
@@ -82,6 +82,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': 'mini_hrd',                      # Or path to database file if using sqlite3.
+    #     'USER': 'root',                      # Not used with sqlite3.
+    #     'PASSWORD': '',                  # Not used with sqlite3.
+    #     'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+    #     'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+    #     'OPTIONS': {
+    #         'sql_mode': 'traditional',
+    #     }
+    # }
 }
 
 
